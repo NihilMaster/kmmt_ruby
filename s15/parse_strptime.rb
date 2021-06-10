303db58f21c72a6477480d94057ba336e9695f34
NihilMaster <mateo.nihil@gmail.com>
2021-06-09 23:53:04 -0400

    Section 15

---
+++ s15/parse_strptime.rb
@@ -0,0 +1,6 @@
+require 'time'
+
+# .parse
+# To use parse it is required to call 'time'
+puts Time.parse("2021-01-01")
+puts Time.strptime("15-04-2021", "%d-%m-%Y")
---
+++ s15/time_obj.rb
@@ -0,0 +1,10 @@
+today = Time.new
+p today
+p today.class
+p Time.now
+puts
+
+p Time.new(2015)
+last_may = Time.new(2015, 4, 15, 17, 20, 12)
+p last_may.class
+puts
